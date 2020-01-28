119225a9d8191c40f56fe72d3ecc89fbbe6ae9d2
Ljakseihc <lyakseihc93@gmail.com>
2020-01-28 22:20:00 +0300

    Initial commit

---
+++ src/main/java/sia/tacocloud/tacos/Ingredient.java
@@ -0,0 +1,15 @@
+package sia.tacocloud.tacos;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+    private String id;
+    private String name;
+    private Type type;
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
